7d6d44d782d7670053a316098e4aa675d6578441
Mohammed Hudair <47048229+Hudair@users.noreply.github.com>
2022-08-23 17:02:52 +0300

    delete omise payment woocommerce

--- wp-content/plugins/omise/includes/libraries/omise-plugin/Omise.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require_once dirname(__FILE__).'/helpers/charge.php';
-require_once dirname(__FILE__).'/helpers/wc_order.php';
-require_once dirname(__FILE__).'/helpers/mailer.php';
-require_once dirname(__FILE__).'/helpers/request.php';
